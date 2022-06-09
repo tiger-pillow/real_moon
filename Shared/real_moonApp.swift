4edb558dffd0e5ef5ea15e53aa1679d90930fccd
gladys <yiyuan.fang@epfl.ch>
2022-06-09 10:42:52 +0200

    Initial Commit

---
+++ Shared/real_moonApp.swift
@@ -0,0 +1,17 @@
+//
+//  real_moonApp.swift
+//  Shared
+//
+//  Created by Gladys on 6/9/22.
+//
+
+import SwiftUI
+
+@main
+struct real_moonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
